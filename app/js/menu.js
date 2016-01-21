ec3053cdd3a51bf4e2e85dd6c950d8199c50d788
Outpox <guillaumeoutpox@gmail.com>
2016-01-21 01:38:39 +0100

    Improved menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -6,19 +6,33 @@ var template = [
     {
         label: 'Language',
         submenu: []
+    },
+    {
+        label: i18n.__('optionsMenu'),
+        submenu: [
+            {
+                label: i18n.__('unlockHardDrive'),
+                click: function () {
+                    unlockHardDrive();
+                }
+            }
+        ]
     }
 ];
 
 var langObject = [];
 Object.keys(i18n.getCatalog()).forEach(function (lang) {
     template[0].submenu.push({
-            click: translate(lang),
-            'label': i18n.__({phrase: 'lang', locale: lang})
-        });
+        click: function () {
+            translate(lang);
+        },
+        'label': i18n.__({phrase: 'lang', locale: lang})
+    });
 });
 
 if (process.platform == 'darwin') {
-    var name = require('electron').app.getName();
+    //var name = require('electron').app.getName();
+    var name = "Liliusm";
     template.unshift({
         label: name,
         submenu: [
@@ -57,20 +71,12 @@ if (process.platform == 'darwin') {
             {
                 label: 'Quit',
                 accelerator: 'Command+Q',
-                click: function() { app.quit(); }
+                click: function () {
+                    app.quit();
+                }
             }
         ]
     });
-    // Window menu.
-    template[3].submenu.push(
-        {
-            type: 'separator'
-        },
-        {
-            label: 'Bring All to Front',
-            role: 'front'
-        }
-    );
 }
 
 var menu = Menu.buildFromTemplate(template);
